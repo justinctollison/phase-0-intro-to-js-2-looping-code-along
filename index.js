2d5c7f9592417d617f52824a1e6bceeded0f98cc
justinctollison <justinctollison@gmail.com>
2021-04-06 14:49:26 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(cards, event)
+{
+    let newArr = [];
+
+    for(let i = 0; i < cards.length; i++)
+    {
+        newArr.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return newArr
+}
+
+writeCards()
+
+function countDown()
+{
+    let countdown = 10;
+    while (countdown >= 0)
+    {
+        console.log(countdown--);
+    }
+}
